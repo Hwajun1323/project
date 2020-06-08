0b1dce7af6882fe9310127f0d6dd461f6bd9d994
J3sven <sven@nevs.me>
2020-06-08 18:52:10 +0200

    fix window resizable menu

--- public/js/hamburger.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#show-hidden-menu').click(function() {
-      $('.hidden-menu').slideToggle("slow");
-      // Alternative animation for example
-      // slideToggle("fast");
-    });
-  });
